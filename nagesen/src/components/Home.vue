f814f89af3af708748b55538d65a07584e262226
yoshiwaki <wakk12107@au.com>
2022-03-05 20:57:38 +0900

    firebase課題４−１

---
+++ nagesen/src/components/Home.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h2>こんにちは {{ name }}さん</h2>
+        <button @click="signOut">サインアウト</button>
+    </div>
+</template>
+
+<script>
+import firebase from '../firebase.js'
+
+export default {
+    data() {
+        return {
+            name: firebase.auth().currentUser.displayName
+        }
+    },
+    methods: {
+        signOut: function () {
+            firebase.auth().signOut().then(() => {
+                this.$router.push('/signin')
+            })
+        }
+    }
+}
+</script>
--- nagesen/src/main.js
+++ nagesen/src/main.js
@@ -1,10 +1,26 @@
+/* eslint-disable */
+
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import firebase from 'firebase/compat/app'
 
 Vue.config.productionTip = false
+
+const config = {
+  apiKey: "AIzaSyCQtHluI82iv-tXahX5Yl7PQTBxS-qlzx4",
+  authDomain: "nagesen12107.firebaseapp.com",
+  projectId: "nagesen12107",
+  storageBucket: "nagesen12107.appspot.com",
+  messagingSenderId: "742240357861",
+  appId: "1:742240357861:web:637dbc64b6ac7b6ec4330c",
+  measurementId: "G-FRW322JP9H"
+};
+
+firebase.initializeApp(config);
+
 
 /* eslint-disable no-new */
 new Vue({
