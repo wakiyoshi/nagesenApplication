a3cf51ee42dadcf14dec22e03d3dd9471552c021
yoshiwaki <wakk12107@au.com>
2022-03-17 16:37:46 +0900

    インデント非同期処理の修正

--- nagesen/src/store/index.js
+++ nagesen/src/store/index.js
@@ -9,61 +9,59 @@ import router from '@/router'
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
-    state: {
-        username: '',
-        email: '',
-        password: '',
-        loginEmail: '',
-        loginPassword: ''
-    },
-    mutations: {
-        AddToState: function (state, payload) {
-            state.email = payload.email
-            state.password = payload.password
-            state.username = payload.username
-            state.loginEmail = payload.loginEmail
-            state.loginPassword = payload.loginPassword
-        }
-    },
-    actions: {
-        signUp: function (context, payload) {
-          firebase.auth().createUserWithEmailAndPassword(payload.email, payload.password)
-            .then(() => {
-              firebase.auth().currentUser.updateProfile({
-                displayName: payload.username,
-              },)
-              .then(() => {
-                context.commit('AddToState', payload)
-              })
-              .then(() => {
-                router.push('/home')
-              })
-              })
-              .catch(error => {
-                console.log(error.message)
-              })
-        },
-        signOut: function () {
-          firebase.auth().signOut().then(() => {
-              router.push('/signin')
-          })
-          .catch(error =>{
-            console.log(error.message)
-          })
-        },
-        signIn: function (context,payload){
-          firebase.auth().signInWithEmailAndPassword(payload.loginEmail,payload.loginPassword)
+  state: {
+    username: '',
+    email: '',
+    password: '',
+    loginEmail: '',
+    loginPassword: ''
+  },
+  mutations: {
+    AddToState: function (state, payload) {
+      state.email = payload.email
+      state.password = payload.password
+      state.username = payload.username
+      state.loginEmail = payload.loginEmail
+      state.loginPassword = payload.loginPassword
+    }
+  },
+  actions: {
+      signUp: function (context, payload) {
+        firebase.auth().createUserWithEmailAndPassword(payload.email, payload.password)
+          .then(() => {
+            firebase.auth().currentUser.updateProfile({
+              displayName: payload.username,
+            },)
             .then(() => {
               context.commit('AddToState', payload)
             })
             .then(() => {
               router.push('/home')
             })
+            })
             .catch(error => {
               console.log(error.message)
             })
-        }
-    },
+      },
+      signOut: function () {
+        firebase.auth().signOut().then(() => {
+            router.push('/signin')
+        })
+        .catch(error =>{
+          console.log(error.message)
+        })
+      },
+      signIn: function (context,payload){
+        firebase.auth().signInWithEmailAndPassword(payload.loginEmail,payload.loginPassword)
+          .then(() => {
+            context.commit('AddToState', payload)
+            router.push('/home')
+          })
+          .catch(error => {
+            console.log(error.message)
+          })
+      }
+  },
 });
 
 export default store
